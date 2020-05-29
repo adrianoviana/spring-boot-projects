2bd4021ffdbb9f0656947c0254e7914f43aff393
adriano <avpinto@tjma.jus.br>
2020-05-28 22:40:11 -0300

    após vídeo requisição put e delete

--- api-bookmanager/src/main/java/com/bookmanager/ws/ui/controllers/BookController.java
+++ api-bookmanager/src/main/java/com/bookmanager/ws/ui/controllers/BookController.java
@@ -1,5 +1,7 @@
 package com.bookmanager.ws.ui.controllers;
 
+import java.util.HashMap;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -21,22 +23,26 @@ import com.bookmanager.ws.ui.model.response.Book;
 @RequestMapping("/books")// http://localhost:8080/books
 public class BookController {
 	
+	HashMap<String, Book> books;
+	
 	@GetMapping
-	public String getBooks(@RequestParam(value="page", defaultValue = "1") int page, 
+	public ResponseEntity<?> getBooks(@RequestParam(value="page", defaultValue = "1") int page, 
 			@RequestParam(value="limit", defaultValue = "50") int limit, 
 			@RequestParam(value="sort", defaultValue = "desc", required = false) String sort) {
-		return "retornar a coleção livros com page = "+page+" e limit = "+limit+" e sort = "+sort;
+		return new ResponseEntity<>(books, HttpStatus.OK);
+		//return "retornar a coleção livros com page = "+page+" e limit = "+limit+" e sort = "+sort;
 	}
 	
 	@GetMapping(path="/{isbn}")
 	public ResponseEntity<Book> getBook(@PathVariable String isbn) {
 		
-		Book book = new Book();
-		book.setTitle("Clean Code");
-		book.setAuthor("Robert C. Martin");
-		book.setImageUrl("http://localhost/image");
+		if(books.containsKey(isbn)) {
+			return new ResponseEntity<Book>(books.get(isbn), HttpStatus.OK);
+		}else {
+			return new ResponseEntity<Book>(HttpStatus.NO_CONTENT);
+		}
 		
-		return new ResponseEntity<Book>(book, HttpStatus.OK);
+		
 	}
 	@PostMapping
 	public ResponseEntity<Book> createBook(@Valid @RequestBody BookDetailRequestModel bookDetail) {
@@ -45,14 +51,28 @@ public class BookController {
 		book.setAuthor(bookDetail.getAuthor());
 		book.setImageUrl(bookDetail.getImageUrl());
 		book.setIsbn(bookDetail.getIsbn());
+		
+		if(books == null) {
+			books = new HashMap<>();
+		}
+		books.put(book.getIsbn(), book);
+		
 		return new ResponseEntity<Book>(book, HttpStatus.OK);
 	}
-	@PutMapping
-	public String updateBook() {
-		return "Atualizar as informações de um determinado livro";
+	@PutMapping(path="/{isbn}")
+	public ResponseEntity<Book> updateBook(@PathVariable String isbn, @Valid @RequestBody BookDetailRequestModel bookDetail) {
+		
+		Book book = books.get(isbn);
+		book.setTitle(bookDetail.getTitle());
+		book.setAuthor(bookDetail.getAuthor());
+		book.setImageUrl(bookDetail.getImageUrl());
+		book.setIsbn(bookDetail.getIsbn());
+		
+		return new ResponseEntity<Book>(book, HttpStatus.OK);
 	}
-	@DeleteMapping
-	public String deleteBook() {
-		return "deletar um livro cadastrado";
+	@DeleteMapping(path="/{isbn}")
+	public ResponseEntity<Void> deleteBook(@PathVariable String isbn) {
+		books.remove(isbn);
+		return ResponseEntity.noContent().build();
 	}
 }
